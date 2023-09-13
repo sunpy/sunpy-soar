8d49209f50896406058a3a07d201be2b06450d3c
Laura Hayes <hayesla@tcd.ie>
2023-09-13 09:25:27 +0200

    Remove the identifier attrs  (#102)
    * removing identifier
    
    * updating tests
    
    * adding changelog
    
    * removing from init

--- sunpy_soar/__init__.py
+++ sunpy_soar/__init__.py
@@ -1,7 +1,7 @@
 # Import here to register the client with sunpy
-from sunpy_soar.attrs import SOOP, Identifier, Product
+from sunpy_soar.attrs import SOOP, Product
 from sunpy_soar.client import SOARClient
 
 from .version import version as __version__
 
-__all__ = ["__version__", "SOARClient", "Product", "Identifier", "SOOP"]
+__all__ = ["__version__", "SOARClient", "Product", "SOOP"]
