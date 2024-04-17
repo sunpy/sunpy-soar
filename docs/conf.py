ced461d5ef5d5b29467c70fc2e42138864844857
CyclingNinja <bennett.sm89@gmail.com>
2024-04-17 17:00:25 +0100

    Registers package with the SunPy template

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+import datetime
 
 # -- Project information -----------------------------------------------------
 
