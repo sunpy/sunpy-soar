5bf8e94242903b0c8f76fbe4d664212248a696e4
NucleonGodX <racerpro41@gmail.com>
2024-06-19 12:00:15 +0530

    both examples merged into one example

--- examples/detector_wavelength.py
+++ examples/detector_wavelength.py
@@ -39,6 +39,29 @@ wavelength = a.Wavelength(580 * u.AA, 640 * u.AA)
 result = Fido.search(instrument & time & level & detector & wavelength)
 result
 #####################################################
+# For instruments PHI and SPICE, we get query results in form of wavemin and wavemax.
+#
+# When two values are provided, it is interpreted as the wavemin and wavemax.
+
+instrument = a.Instrument("SPICE")
+time = a.Time("2023-02-01 2:00", "2023-02-01 2:30")
+level = a.Level(2)
+detector = a.Detector("SW")
+wavelength = a.Wavelength(69.523026 * u.AA, 79.508766 * u.AA)
+#####################################################
+# Now do the search.
+
+result = Fido.search(instrument & time & level & wavelength & detector)
+result
+#####################################################
+# When a single value given for wavelength is interpreted as the wavemin.
+
+
+wavelength = a.Wavelength(69.523026 * u.AA)
+result = Fido.search(instrument & time & level & wavelength & detector)
+result
+
+#####################################################
 # Finally we can download the data.
 #
 # For this example, we will comment out the download part
