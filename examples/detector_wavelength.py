60b620f3425103ac60c6ebd64b2e1cf57af87e6e
NucleonGodX <racerpro41@gmail.com>
2024-06-22 00:33:39 +0530

    how-to guide added and example updated

--- examples/detector_wavelength.py
+++ examples/detector_wavelength.py
@@ -16,9 +16,6 @@ import sunpy_soar  # NOQA: F401
 
 #####################################################
 # We shall start with constructing a search query with wavelength and detector.
-# For instruments EUI, METIS and SOLOHI we get query results in form of wavelength.
-#
-# When a single wavelength is provided it is interpreted as the wavelength value.
 
 instrument = a.Instrument("METIS")
 time = a.Time("2023-02-01 01:00", "2023-02-01 05:00")
@@ -29,36 +26,6 @@ wavelength = a.Wavelength(121.6 * u.AA)
 # Now do the search.
 
 result = Fido.search(instrument & time & level & detector & wavelength)
-result
-
-#####################################################
-# When a range of wavelength is provided, it is interpreted as the wavemin and wavemax.
-
-detector = a.Detector("VLD")
-wavelength = a.Wavelength(580 * u.AA, 640 * u.AA)
-result = Fido.search(instrument & time & level & detector & wavelength)
-result
-#####################################################
-# For instruments PHI and SPICE, we get query results in form of wavemin and wavemax.
-#
-# When two values are provided, it is interpreted as the wavemin and wavemax.
-
-instrument = a.Instrument("SPICE")
-time = a.Time("2023-02-01 2:00", "2023-02-01 2:30")
-level = a.Level(2)
-detector = a.Detector("SW")
-wavelength = a.Wavelength(69.523026 * u.AA, 79.508766 * u.AA)
-#####################################################
-# Now do the search.
-
-result = Fido.search(instrument & time & level & wavelength & detector)
-result
-#####################################################
-# When a single value given for wavelength is interpreted as the wavemin.
-
-
-wavelength = a.Wavelength(69.523026 * u.AA)
-result = Fido.search(instrument & time & level & wavelength & detector)
 result
 
 #####################################################
