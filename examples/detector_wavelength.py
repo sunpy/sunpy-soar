df17fa986fbcd618e897d559f1f57d7bb8712c34
NucleonGodX <racerpro41@gmail.com>
2024-06-22 12:54:07 +0530

    all suggestions applied

--- examples/detector_wavelength.py
+++ examples/detector_wavelength.py
@@ -10,12 +10,11 @@ import astropy.units as u
 import sunpy.net.attrs as a
 from sunpy.net import Fido
 
-#####################################################
+###############################################################################
 # Importing sunpy_soar registers the client with sunpy
-
 import sunpy_soar  # NOQA: F401
 
-#####################################################
+###############################################################################
 # We shall start with constructing a search query with wavelength and detector.
 
 instrument = a.Instrument("METIS")
@@ -24,9 +23,8 @@ level = a.Level(2)
 detector = a.Detector("UVD")
 wavelength = a.Wavelength(121.6 * u.AA)
 
-#####################################################
+###############################################################################
 # Now do the search.
 
 result = Fido.search(instrument & time & level & detector & wavelength)
 print(result)
-
