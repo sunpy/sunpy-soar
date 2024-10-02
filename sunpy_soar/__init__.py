81ef6699ea2bba0fc3023381f3b5e863fda2fbb2
hayesla <hayesla@tcd.ie>
2024-10-02 10:12:56 +0100

    adding distance to init

--- sunpy_soar/__init__.py
+++ sunpy_soar/__init__.py
@@ -9,9 +9,9 @@ A sunpy plugin for accessing data in the Solar Orbiter Archive (SOAR).
 * Source Code: https://github.com/sunpy/sunpy-soar
 """
 
-from sunpy_soar.attrs import SOOP, Product
+from sunpy_soar.attrs import SOOP, Product, Distance
 from sunpy_soar.client import SOARClient  # Import here to register the client with sunpy
 
 from .version import version as __version__
 
-__all__ = ["__version__", "SOARClient", "Product", "SOOP"]
+__all__ = ["__version__", "SOARClient", "Product", "SOOP", "Distance"]
